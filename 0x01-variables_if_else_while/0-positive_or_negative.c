eaf483a27869eba9e7fb817de384e9eea8b8d2ac
Muawia24 <amuawia666@gmail.com>
2023-12-15 15:38:29 -0800

    Last edit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,10 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - knoing numbers wheather its positive or not
+ * Return: 0 always
+ */
 int main(void)
 {
 	int n;
