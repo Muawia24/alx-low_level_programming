2aa22e0dec8dc71fad37d6384e289ef2621dcc3b
Muawia24 <amuawia666@gmail.com>
2023-12-22 05:02:04 -0800

    Fixin bugs

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "0-positive_or_negative.c"
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include "main.h"
+
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
