e6830d75e86e7703d45ad3522fa095b2a37c7ffc
Muawia24 <amuawia666@gmail.com>
2024-01-18 03:35:04 -0800

    1-string_concat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,17 +13,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat;
 	unsigned int i, k, l;
 
-	if(s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	i = 0;
-	
 	while (s1[i] != '\0')
 		i++;
 
 	concat = malloc(sizeof(char) * (n + i + 1));
-	
+
 	if (concat == NULL)
 		return (NULL);
 
