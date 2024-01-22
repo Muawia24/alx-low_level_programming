6a553fa964db9a732af4a1d54d9e6ddfcd62b34f
Muawia24 <amuawia666@gmail.com>
2024-01-22 14:30:37 -0800

    Dog struct commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+#include <stdio.h>
+
+/**
+ * struct dog - dog information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif  /*DOG*/
