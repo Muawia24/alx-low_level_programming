bd45d681b012ada64630ff1b5034b23e52baf7b6
Muawia24 <amuawia666@gmail.com>
2024-02-01 11:52:53 -0800

    implimented function to print all nodes in a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ * print_list - function that prints all the
+ *  elements of a list_t list.
+ *  @h: pointer to the head of list
+ *  Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i;
+	const list_t *current = h;
+
+	for (i = 0; current != NULL; i++)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			current = current->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", current->len, current->str);
+			current = current->next;
+		}
+	}
+	return (i);
+}
