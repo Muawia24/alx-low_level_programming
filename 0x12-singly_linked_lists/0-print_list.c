55c9ac273fbff62685cef0f6c3a8c44839d8454f
Muawia24 <amuawia666@gmail.com>
2024-02-01 12:38:40 -0800

    Fixed betty code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ size_t print_list(const list_t *h)
 {
 	int i;
 	const list_t *current = h;
-	
+
 	i = 0;
 	while (current != NULL)
 	{
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", current->len, current->str);
-		
+
 		i++
 		current = current->next;
 	}
