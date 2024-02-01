ad5bcc214fe6e13f9ad8d019b711c225e5445c54
Muawia24 <amuawia666@gmail.com>
2024-02-01 12:36:53 -0800

    Fixed the counter index

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,19 +10,17 @@ size_t print_list(const list_t *h)
 {
 	int i;
 	const list_t *current = h;
-
-	for (i = 0; current != NULL; i++)
+	
+	i = 0;
+	while (current != NULL)
 	{
 		if (current->str == NULL)
-		{
 			printf("[0] (nil)\n");
-			current = current->next;
-		}
 		else
-		{
 			printf("[%d] %s\n", current->len, current->str);
-			current = current->next;
-		}
+		
+		i++
+		current = current->next;
 	}
 	return (i);
 }
