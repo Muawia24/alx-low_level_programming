a576298aaaf0c32ef288acd8981a5c3ed31cb7c0
Muawia24 <amuawia666@gmail.com>
2024-02-07 17:50:08 -0800

    Errors handeling & betty code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 /**
+ * listint_len - returns the number of elements in a linked list
+ * @h: pointer to the head of a list
+ *
+ * Return: numbers of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		nnodes++;
+		h = h->next;
+	}
+	return (nnodes);
+}
+/**
  * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: head of list
  * @idx: index where new node shoul be inserted
