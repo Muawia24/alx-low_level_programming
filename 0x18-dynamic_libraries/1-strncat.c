dbfdc7be02e36427838297eaeb15edb5f189820f
Muawia24 <amuawia666@gmail.com>
2024-03-25 16:15:54 -0700

    libdynamic.so

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - Write a function that concatenates two strings. it will use at
+ *            most n bytes from src
+ *
+ * @dest: This is the output  dest
+ * @src: This is the input src
+ * @n: This is the number bytes fro src
+ *
+ * Return: This is my return
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[j] = src[i];
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*_strstr - The _strstr() function finds the first occurrence
+* of the substring needle in the string haystack.
+* The terminating null bytes (\0) are not compared
+*@haystack: string where the search is made
+*@needle: string whose occurence is searched in haystack
+*Return:Returns a pointer to the beginning of the located
+* substring, or NULL if the substring is not found.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *sub;
+
+	while (*haystack)
+	{
+		sub = haystack;
+
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+		if (*needle == '\0')
+		{
+			return (sub);
+		}
+		haystack++;
+
+	}
+	return (NULL);
+}
