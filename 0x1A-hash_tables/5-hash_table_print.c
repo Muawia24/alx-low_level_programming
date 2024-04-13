60e27ca0d10e7fe7bd71e0c67b94252becc779f1
Muawia24 <amuawia666@gmail.com>
2024-04-13 06:16:46 -0700

    5-hash_table_print.c debuged

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,19 +7,30 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
+	hash_node_t *node;
 	int flag = 0;
 
 	if (ht == NULL)
 		return;
 
+
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] && flag == 1)
-			printf(", ");
 		if (ht->array[i])
 		{
-			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			if (flag == 1)
+				printf(", ");
+
+			node = ht->array[i];
+			while (node)
+			{
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				node = node->next;
+
+				if (node != NULL)
+					printf(", ");
+			}
 
 			flag = 1;
 		}
