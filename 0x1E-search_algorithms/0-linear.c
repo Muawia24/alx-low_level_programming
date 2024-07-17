347304a5cc8f170800528c9b222eb9fa9e3c788a
Ahmed Muawia <amuawia666@gmail.com>
2024-07-17 04:32:50 +0300

    Fixed index error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < ln - 1; i++)
+	for (i = 0; i < ln; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
